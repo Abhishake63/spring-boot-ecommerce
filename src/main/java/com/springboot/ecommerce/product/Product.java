70e74dbf59e3b28bbee92e197697c2ba8a326b47
Abhishake Sen <abhishake63@student.sust.edu>
2024-04-27 20:43:50 +0600

    added necessary annotations for req args constructor in product class

--- src/main/java/com/springboot/ecommerce/product/Product.java
+++ src/main/java/com/springboot/ecommerce/product/Product.java
@@ -1,16 +1,17 @@
 package com.springboot.ecommerce.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.springboot.ecommerce.order.OrderItem;
+import com.springboot.ecommerce.wishlist.Wishlist;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "products")
@@ -18,16 +19,29 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private Long id;
 
     @Column(nullable = false)
+    @NonNull
     private String name;
 
+    @NonNull
     private String description;
 
+    @NonNull
     @Column(nullable = false)
     private BigDecimal price;
 
+    @NonNull
     @Column(nullable = false)
     private Integer stockQuantity;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<OrderItem> orderItems;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Wishlist> wishlist;
 }
